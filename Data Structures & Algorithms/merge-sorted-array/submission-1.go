78b9371c437654e0993a6ee25963b3c29cd8e3e8
Ajay Kumbhar <48400669+0xpica@users.noreply.github.com>
2026-04-07 09:33:24 +0530

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.go
@@ -0,0 +1,26 @@
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i := m - 1
+	j := n - 1 
+	ptr := m + n - 1
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[ptr] = nums1[i]
+			i--
+		}else{
+			nums1[ptr] = nums2[j]
+			j--
+		}
+		ptr--
+	}
+	for i >= 0 {
+		nums1[ptr] = nums1[i]
+		ptr--
+		i--
+	}
+	for j >= 0 {
+		nums1[ptr] = nums2[j]
+		ptr--
+		j--
+	}
+}
+
